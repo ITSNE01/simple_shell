a6e2446f1f4e1ef25f6289b9727d5ce8bf764934
Meryem <ml.meryem.16@gmail.com>
2024-08-26 02:37:20 -0700

    bett code

---
+++ betty.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "Hello, I am a code that passes the Betty check."
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("Hello, I am a code that passes the Betty check.\n");
+    return (0);
+}
+
